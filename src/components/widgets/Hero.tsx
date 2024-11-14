c05341594f8120489a91b3ba8186d55a5c6bbcda
jvm3 <jamesandrewpike@gmail.com>
2024-11-14 18:38:28 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -22,7 +22,7 @@ export default component$((props: { theme?: string }) => {
         </div>
         <div class="relative max-w-7xl mx-auto lg:px-12 lg:gap-20 lg:mt-8 px-4 sm:px-6 flex flex-col items-center text-center lg:flex-row lg:items-center lg:text-left">
           <div class="lg:basis-1/2 pb-10 md:pb-16">
-            <h1 class="text-5xl md:text-6xl text-white sm:text-gray-900 font-bold leading-tighter tracking-tighter mb-4 font-heading">
+            <h1 class="text-5xl md:text-6xl text-primary font-bold leading-tighter tracking-tighter mb-4 font-heading">
               Draw Outside the Lines with
               <br class="hidden lg:block" />
               <span class="text-primary">Etchy Sketchy</span>
@@ -32,14 +32,14 @@ export default component$((props: { theme?: string }) => {
             </p>
             <div class="sm:max-w-md flex flex-col sm:flex-row sm:justify-center gap-4 lg:justify-start">
               <a
-                class="btn bg-primary-500 text-white font-bold w-full"
+                class="btn bg-primary  text-white font-bold w-full"
                 href="https://aymansor.github.io/Etch-a-Sketch/"
                 target="_blank"
                 rel="noopener"
               >
                 Start Etching
               </a>
-              <button class="btn w-full bg-gray-50">Learn More</button>
+              <button class="btn w-full  bg-secondary">Learn More</button>
             </div>
           </div>
           <div class="lg:basis-1/2">
