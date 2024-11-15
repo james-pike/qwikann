e5f89b8efce89867d87ba6e4055b09bb8142c05a
jvm3 <jamesandrewpike@gmail.com>
2024-11-15 12:15:24 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -19,26 +19,26 @@ export default component$((props: { theme?: string }) => {
           />
         </div>
         <div class="relative max-w-7xl mx-auto lg:px-12 lg:gap-20 lg:mt-8 px-4 sm:px-6 flex flex-col items-center text-center lg:flex-row lg:items-center lg:text-left">
-          <div class="lg:basis-1/2 pb-10 md:pb-16">
-            <h1 class="text-5xl md:text-6xl text-secondary font-bold leading-tighter tracking-tighter mb-4 font-heading">
+          <div class="lg:basis-1/2 pb-60 md:pb-16">
+            <h1 class="text-5xl md:text-6xl text-primary font-bold leading-tighter tracking-tighter mb-4 font-heading">
               Build A Better {""}
               <br class="hidden lg:block" />
-              <span class="text-accent">Website {""}</span>
+              <span class="text-accent">Premium Website {""}</span>
              
             </h1>
-            <p class="text-xl text-muted mb-6 text-primary max-w-3xl mx-auto lg:max-w-none">
+            <p class="text-xl text-muted mb-6 pt-1 text-primary max-w-3xl mx-auto lg:max-w-none">
               Master the lines, embrace the quirks, and turn every twist into a masterpiece—because art's all about the journey, not the perfect line.
             </p>
-            <div class="sm:max-w-md flex flex-col sm:flex-row sm:justify-center gap-4 lg:justify-start">
+            <div class="sm:max-w-md flex flex-col pt-1 sm:flex-row sm:justify-center gap-4 lg:justify-start">
               <a
-                class="btn bg-primary  text-white font-bold w-full"
+                class="btn bg-primary p-3 text-white font-bold w-full"
                 href="https://aymansor.github.io/Etch-a-Sketch/"
                 target="_blank"
                 rel="noopener"
               >
                 Start Etching
               </a>
-              <button class="btn w-full bg-accent">Learn More</button>
+              <button class="btn w-full p-3 bg-secondary">Learn More</button>
             </div>
           </div>
           <div class="lg:basis-1/2">
